91c5f390f52883c9c11f1265625fead9059504fd
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-01-11 18:18:58 +0100

    display filename & correct linenumber at stacktrace

--- test/lib/scriptContext.js
+++ test/lib/scriptContext.js
@@ -26,6 +26,23 @@ describe('scriptContext', () => {
             expect(allExecuted).to.equal(false);
         });
 
+        it('should return the correct line number at error', () => {
+            const context = scriptContext({}, [{
+                file: 'file.js',
+                content: `throw Error('NOPE')`
+            }]);
+
+            const [script] = context.scriptResult;
+            const { error } = script;
+            const { stack } = error;
+
+            const result = /at .*:(\d+):(\d+)/.exec(stack);
+            expect(result).to.be.not.equal(null);
+
+            const line = Number(result[1]);
+            expect(line).to.equal(1);
+
+        });
     });
 
 });
